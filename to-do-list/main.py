33ecb7385ec83f629ca7f15f5f25a722961844b3
ejapten <ejapten@gmail.com>
2025-08-18 11:29:59 +0700

    Update pada logika dan merapikannya pada fungsi add_task() lalu menambkan input untuk keluar dari program dengan cara mengetik opsi simpan atau tidak simpan, jika simpan dia keluar sistem, jika tidak simpan dia masukan input tanggal dan nama tugas lagi, jika ketik selain simpan atau tidak simpan maka proses pengetikan akan diulang kembali

--- to-do-list/main.py
+++ to-do-list/main.py
@@ -3,13 +3,16 @@ from datetime import datetime
 # Menambah Tugas
 def add_task():
   todo_list = {}
-  print("Format date and time -> Years-Month-Day Hours:Minute:Second")
+  print("Format date and time -> Years-Month-Day")
 
   while True:
+
+    # Input Tanggal
     while True:
       try:
         enter_date_task = input("Masukan Tanggal : ").strip()
-        format_date = '%Y-%m-%d %H:%M:%S'
+        # Format Tanggal
+        format_date = '%Y-%m-%d'
         date_task = datetime.strptime(enter_date_task, format_date)
         todo_list['Date'] = date_task
         break
@@ -18,12 +21,25 @@ def add_task():
         print(f"Format tanggal anda '{enter_date_task}' Salah, masukan dalam bentuk format yang benar")
         continue
 
+    # Input Nama tugas yang akan dikerjakan 
     enter_task = input("Masukan tugas : ")
     if enter_task == 'end':
       break
-
     todo_list['Task'] = enter_task
 
+    # Input Lanjutan untuk simpan
+    while True : 
+      input_simpan = input("Apakah ingin disimpan? ")
+      if input_simpan.lower() == "simpan":
+        exit()
+      elif input_simpan.lower() == "tidak simpan":
+        break
+      else:
+        print("Masukan dengan benar")
+        continue
+      
+          
+          
 
   # Menampilkan output tanggal dalam bentuk 'contoh : 2009-12-23 14:40:12'
   # print(f"Task : {todo_list['Task']}")
