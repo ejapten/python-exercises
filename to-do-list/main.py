211f00e0405eef2cae406d7c3af448a2a403981e
ejapten <ejapten@gmail.com>
2025-08-22 12:31:15 +0700

    Update: Menambahkan penomoran dan mengatur tampilan

--- to-do-list/main.py
+++ to-do-list/main.py
@@ -2,8 +2,10 @@ from datetime import datetime
 
 daftar_tugas = {}
 
+
 # Menambah Tugas
 def add_task(todo_list):
+  
   print("Format date and time -> Years-Month-Day")
 
   while True:
@@ -29,25 +31,32 @@ def add_task(todo_list):
     # Menambahkan ke dalam penyimpanan
     if date_task in todo_list:
       print(f"\n-->Tanggal {date_task.strftime('%Y-%m-%d')} sudah ada, menambahkan tugas baru.")
-      todo_list[date_task].append(enter_task)
+      nomor = len(todo_list[date_task]) + 1
+      todo_list[date_task][nomor] = enter_task
     else:
       print(f"\n-->Membuat entri baru untuk tanggal {date_task.strftime('%Y-%m-%d')}.")
-      todo_list[date_task] = [enter_task] 
-
+      todo_list[date_task] = {1: enter_task}
 
     # Input Lanjutan untuk simpan
-    while True : 
-      input_simpan = input("\nApakah ingin disimpan?\n")
-      if input_simpan.lower() == "simpan":
-        for key, task_list in todo_list.items():
-          tasks_str = ", ".join(task_list) # ubah list menjadi string dengan dipisahkan koma dan spasi
-          print(f"Tanggal: {key.strftime('%Y-%m-%d')} | Tugas: {tasks_str}")
-        return todo_list
-      elif input_simpan.lower() == "tidak simpan":
-        break
-      else:
-        print("\nMasukan dengan benar")
-        continue
+    while True: 
+        input_simpan = input("\nApakah ingin disimpan (Y/N) ?\n")
+
+        if input_simpan.lower() == "y":
+            print("\n"+"="*35)
+            print("Berikut Daftar Tugas anda yang telah ditambahkan: ")
+            for key, task_dict in todo_list.items():
+                print(f"\nTanggal: {key.strftime('%Y-%m-%d')}")
+                for num, task in task_dict.items():
+                    print(f"  {num}. {task}")
+            print("\n"+"="*35)
+            return todo_list
+            
+        elif input_simpan.lower() == "n":
+            break
+
+        else:
+            print("\nMasukan dengan benar")
+            continue
 
   return todo_list
 
@@ -57,9 +66,14 @@ def view_task() :
     if not daftar_tugas:   # kalau kosong
         print("\nBelum ada tugas.")
     else:
-        for key, task_list in daftar_tugas.items():
-            tasks_str = ", ".join(task_list)
-            print(f"Tanggal: {key.strftime('%Y-%m-%d')} | Tugas: {tasks_str}")
+        for date, tasks in daftar_tugas.items():
+          print(f"\nTanggal: {date.strftime('%Y-%m-%d')}")
+          for num, task in tasks.items():
+            print(f"  {num}. {task}")
+
+# Fungsi Menghapus pada Daftar tugas
+#def delete_task():
+  # for key, task_list in daftar_tugas.items():
 
 while True:
   print("\n" + "-"*20)
@@ -73,8 +87,13 @@ while True:
     daftar_tugas = add_task(daftar_tugas)
   elif pilihan_menu == "2":
     view_task()
-  else:
+  elif pilihan_menu == "3":
     break
+  else:
+    print("\n--> Masukan pilihan dengan tepat")
+    continue
+    
+     
 
 
 
