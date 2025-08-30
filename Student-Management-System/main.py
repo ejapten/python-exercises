a266f4b6a79997da5569667d1f13eb25a6aeb8a8
ejapten <ejapten@gmail.com>
2025-08-30 16:22:02 +0700

    Update : Menambahkan fungsi identitas siswa di sekolah

--- Student-Management-System/main.py
+++ Student-Management-System/main.py
@@ -1,4 +1,6 @@
 # Menambahkan nama, umur, kelas, nilai, ID
+# indo, inggris, mtk, biologi, kimia, fisika, sejarah = IPA
+# indo, inggris, mtk, ekonomi, geografi, sosiologi, sejarah = IPS
 
 import re
 
@@ -24,7 +26,31 @@ def identitas_pribadi_siswa():
       break
     else:
         print("Masukan umur yang benar")
+
+def identitas_siswa_di_sekolah():
+
+  daftar_kelas = ["X IPA 1", "X IPA 2", "X IPA 3", "X IPS 1", "X IPS 2", "X IPS 3",
+                 "XI IPA 1", "XI IPA 2", "XI IPA 3", "XI IPS 1", "XI IPS 2", "XI IPS 3",
+                 "XII IPA 1", "XII IPA 2", "XII IPA 3", "XII IPS 1", "XII IPS 2", "XII IPS 3"]
+  
+  while True:
+    nis_siswa = input("Masukan NIS : ")
+
+    if nis_siswa.isdigit():
+      break
+    else:
+      print("NIS siswa tidak boleh mengandung angka dan simbol")
+  
+  while True:
+    kelas_siswa = input("Masukan Kelas : ").strip().lower()
+    
+    for kelas in daftar_kelas:
+      if kelas_siswa == kelas.lower():
+        print("Kelas Berhasil ditemukan")
+        return True
+    else:
+      print("Kelas tidak berhasil ditemukan")
     
 
-identitas_pribadi_siswa()
+identitas_siswa_di_sekolah()
 
